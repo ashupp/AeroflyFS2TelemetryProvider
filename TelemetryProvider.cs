2c45c618b3d469369ecd1ea4872b7c38f0dd1881
Alexander Hupp <ahupp@ashnet.de>
2020-05-28 17:23:36 +0200

    First test with interpolating values

--- TelemetryProvider.cs
+++ TelemetryProvider.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
 using System.IO;
@@ -9,6 +10,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Timers;
+using static System.Decimal;
 using ILogger = SimFeedback.log.ILogger;
 
 namespace SimFeedback.telemetry
@@ -63,12 +65,21 @@ namespace SimFeedback.telemetry
             }
         }
 
+        public static double lerp(double a, double b, double f)
+        {
+            return a + f * (b - a);
+        }
+
+        TelemetryData lastTelemetryData = new TelemetryData();
+        TelemetryData telemetryDataA = new TelemetryData();
+        TelemetryData telemetryDataB = new TelemetryData();
+        Queue<TelemetryData> telemetryQueue = new Queue<TelemetryData>();
+        Stopwatch packetStopwatch = new Stopwatch();
+
         private void Run()
         {
-            TelemetryData lastTelemetryData = new TelemetryData();
-            TelemetryData telemetryDataA = new TelemetryData();
-            TelemetryData telemetryDataB = new TelemetryData();
-            
+   
+
             UdpClient socket = new UdpClient {ExclusiveAddressUse = true};
             socket.Client.Bind(new IPEndPoint(IPAddress.Any,_portNum));
             var endpoint = new IPEndPoint(IPAddress.Parse(_ipAddr), _portNum);
@@ -77,34 +88,33 @@ namespace SimFeedback.telemetry
             int emptyCount = -1;
 
             var mmTimer = new System.Timers.Timer();
-            mmTimer.Interval = 10;
+            mmTimer.Interval = 100;
 
+            mmTimer.Elapsed += MmTimer_Elapsed1;
 
-            // 100 Hz Timer
-            Random rnd = new Random();
-            
+            /*
             mmTimer.Elapsed += delegate(object o, ElapsedEventArgs args)
             {
                 // Dieser Timer schlägt alle 10 Millisekunden zu. Hier sollten die neuesten Daten verfügbar sein.
 
+                var telemetryQueueCount = telemetryQueue.Count;
 
-                //TelemetryData telemetryData = ParseReponse(Encoding.UTF8.GetString(socket.Receive(ref endpoint)));
-                TelemetryData telemetryData = new TelemetryData();
-                telemetryData.Pitch = rnd.Next(-90, 90);
-                    var argsx = new TelemetryEventArgs(new AeroflyFS2TelemetryInfo(telemetryData, lastTelemetryData));
-                RaiseEvent(OnTelemetryUpdate, argsx);
-                lastTelemetryData = telemetryData;
-            };
+                if (telemetryQueueCount > 0)
+                {
+                    File.AppendAllText("D:\\test.txt", DateTime.Now + ": " + DateTime.Now.Ticks + " -  Queue Count " + telemetryQueueCount + Environment.NewLine);
+                    RaiseEvent(OnTelemetryUpdate, new TelemetryEventArgs(new AeroflyFS2TelemetryInfo(telemetryQueue.Dequeue(), lastTelemetryData)));
+                }
+                else
+                {
+                    File.AppendAllText("D:\\test.txt", DateTime.Now + ": " + DateTime.Now.Ticks + " -  Keine Daten mehr in Queue" + Environment.NewLine);
+                }
+            };*/
 
 
 
             // Telemetriedaten entgegennehmen
             // Eine Stoppuhr verwenden 
-
-
-
-
-
+            
 
             //mmTimer.Start();
             IsConnected = true;
@@ -127,59 +137,55 @@ namespace SimFeedback.telemetry
                          }
                          continue;
                      }
-                     IsConnected = true;
-                     IsRunning = true;
-                     
+                    IsConnected = true;
+                    IsRunning = true;
 
 
-                            // Hier kommen die Daten rein
-                            
-                            TelemetryData currentTelemetryData = ParseReponse(Encoding.UTF8.GetString(socket.Receive(ref endpoint)));
-                            if (currentTelemetryData.isEmptyTelemetry())
-                            {
-                                File.AppendAllText("D:\\test.txt", DateTime.Now + ": " + DateTime.Now.Ticks + " -  is empty" + Environment.NewLine);
-                                if (emptyCount == -1)
-                                {
-                                    File.AppendAllText("D:\\test.txt", DateTime.Now + ": " + DateTime.Now.Ticks + " -  was -1 will be 1" + Environment.NewLine);
-                                    emptyCount = 1;
-                                }
-                                else
-                                {
-                                    File.AppendAllText("D:\\test.txt", DateTime.Now + ": " + DateTime.Now.Ticks + " -  will be counted up " + emptyCount + Environment.NewLine);
-                                    emptyCount++;
-                                }
-                            }
-                            else
-                            {
-                                File.AppendAllText("D:\\test.txt", DateTime.Now + ": " + DateTime.Now.Ticks + " -  is not empty" + Environment.NewLine);
-
-                                if (telemetryDataA.isSet)
-                                {
-                                    File.AppendAllText("D:\\test.txt", DateTime.Now + ": " + DateTime.Now.Ticks + " -  setzte telemetry data B - hier beginnen auszugeben und auf null setzen" + Environment.NewLine);
-                                    telemetryDataB = currentTelemetryData;
-                                    telemetryDataA.isSet = false;
-
-                                    // Puffer voll - ausgeben
-
-                                    emptyCount = -1;
-                                }
-                                else
-                                {
-                                    File.AppendAllText("D:\\test.txt", DateTime.Now + ": " + DateTime.Now.Ticks + " -  setzte telemetry data A" + Environment.NewLine);
-                                    telemetryDataA = currentTelemetryData;
-                                }
+                    // Hier kommen die Daten rein
 
 
-                            }
+
+                    // Alle Daten sind immer gefüllt
 
 
-                            /*
-                            TelemetryData telemetryData = ParseReponse(Encoding.UTF8.GetString(socket.Receive(ref endpoint)));
-                            var args = new TelemetryEventArgs(new AeroflyFS2TelemetryInfo(telemetryData, lastTelemetryData));
-                            RaiseEvent(OnTelemetryUpdate, args);
-                            lastTelemetryData = telemetryData;
-                            */
-                            sw.Restart();
+                    // Der Erste Datensatz ist noch nicht gesetzt
+                    if (!telemetryDataA.isSet)
+                    {
+                        File.AppendAllText("D:\\test.txt", DateTime.Now + ": " + DateTime.Now.Ticks + " -  setzte telemetry data A" + Environment.NewLine);
+                        telemetryDataA = ParseReponse(Encoding.UTF8.GetString(socket.Receive(ref endpoint)));
+                        packetStopwatch.Start();    // Erster Datensatz gelesen, warte auf zweiten
+                        
+                    }
+                    else
+                    {
+                        // Der Erste Datensatz war bereits gesetzt.
+                        packetStopwatch.Stop();
+                        var elapsedMillisecondsBetweenPackets = packetStopwatch.ElapsedMilliseconds;
+                        File.AppendAllText("D:\\test.txt", DateTime.Now + ": " + DateTime.Now.Ticks + " -  setzte telemetry data B (" + elapsedMillisecondsBetweenPackets + " Millisekunden später) " + Environment.NewLine );
+                        telemetryDataB = ParseReponse(Encoding.UTF8.GetString(socket.Receive(ref endpoint)));
+
+                        // Hier alle fehlenden Daten dazwischen interpolieren
+                        // TODO: Evtl. direkt in dieser Funktion aufdie Queue schieben, ohne die unten folgende Schleife
+                        List<TelemetryData> interpolatedElems = interpolateBetween(telemetryDataA, telemetryDataB, elapsedMillisecondsBetweenPackets);
+
+                        // Auf die Queue packen
+                        foreach (var interpolatedElem in interpolatedElems)
+                        {
+                            telemetryQueue.Enqueue(interpolatedElem);
+                            File.AppendAllText("D:\\test.txt", DateTime.Now + ": " + DateTime.Now.Ticks + " -  Füge generierten Datensatz ein..." + Environment.NewLine);
+                        }
+
+                        // Nach dem interpolieren Am Ende B nach A kopieren.
+                        telemetryDataA = telemetryDataB;
+                        telemetryDataA.isSet = false;
+                        packetStopwatch.Reset();
+
+                        // Nach dem ersten interpolieren eigene Frequenz starten:
+                        mmTimer.Start();
+
+                    }
+
+                    sw.Restart();
                 }
                 catch (Exception e)
                 {
@@ -189,8 +195,52 @@ namespace SimFeedback.telemetry
                     Thread.Sleep(1000);
                 }
              }
-            //IsConnected = false;
-            //IsRunning = false;
+            IsConnected = false;
+            IsRunning = false;
+        }
+
+        private void MmTimer_Elapsed1(object sender, ElapsedEventArgs e)
+        {
+            // Dieser Timer schlägt alle 10 Millisekunden zu. Hier sollten die neuesten Daten verfügbar sein.
+
+            var telemetryQueueCount = telemetryQueue.Count;
+
+            if (telemetryQueueCount > 0)
+            {
+                File.AppendAllText("D:\\test.txt", DateTime.Now + ": " + DateTime.Now.Ticks + " -  Queue Count " + telemetryQueueCount + Environment.NewLine);
+                RaiseEvent(OnTelemetryUpdate, new TelemetryEventArgs(new AeroflyFS2TelemetryInfo(telemetryQueue.Dequeue(), lastTelemetryData)));
+            }
+            else
+            {
+                File.AppendAllText("D:\\test.txt", DateTime.Now + ": " + DateTime.Now.Ticks + " -  Keine Daten mehr in Queue" + Environment.NewLine);
+            }
+        }
+
+        private List<TelemetryData> interpolateBetween(TelemetryData telemetryDataA, TelemetryData telemetryDataB, long elapsedMillisecondsBetweenPackets)
+        {
+            // hier alle Werte durchgehen und interpolieren
+
+            List<TelemetryData> interpolatedElems = new List<TelemetryData>();
+
+            var missingElems = (double)Floor(elapsedMillisecondsBetweenPackets / 10);
+
+            for (int i = 0; i <= missingElems; i++)
+            {
+                var interpolatedTelemetryData = new TelemetryData();
+                interpolatedTelemetryData = telemetryDataA;
+
+                interpolatedTelemetryData.Pitch = (float)lerp(telemetryDataA.Pitch, telemetryDataB.Pitch, i / missingElems);
+                interpolatedTelemetryData.Roll = (float) lerp(telemetryDataA.Roll, telemetryDataB.Roll, i / missingElems);
+                interpolatedTelemetryData.Yaw = (float)lerp(telemetryDataA.Yaw, telemetryDataB.Yaw, i / missingElems);
+                interpolatedTelemetryData.Sway = (float) lerp(telemetryDataA.Sway, telemetryDataB.Sway, i / missingElems);
+                interpolatedTelemetryData.Surge = (float)lerp(telemetryDataA.Surge, telemetryDataB.Surge, i / missingElems);
+                interpolatedTelemetryData.Heave = (float) lerp(telemetryDataA.Heave, telemetryDataB.Heave, i / missingElems);
+                interpolatedTelemetryData.AirSpeed = (float) lerp(telemetryDataA.AirSpeed, telemetryDataB.AirSpeed, i / missingElems);
+                interpolatedTelemetryData.GroundSpeed = (float)lerp(telemetryDataA.GroundSpeed, telemetryDataB.GroundSpeed, i / missingElems);
+
+                interpolatedElems.Add(interpolatedTelemetryData);
+            }
+            return interpolatedElems;
         }
 
         private void MmTimer_Elapsed(object sender, MultimediaElapsedEventArgs e)
@@ -201,6 +251,7 @@ namespace SimFeedback.telemetry
         private TelemetryData ParseReponse(string resp)
         {
             File.AppendAllText("D:\\test.txt", DateTime.Now + ": " + DateTime.Now.Ticks + " - " + resp + Environment.NewLine);
+            // TODO: telemetryData könnte auch direkt A oder B sein
             TelemetryData telemetryData = new TelemetryData();
             string[] lines = resp.Split(';');
             telemetryData.Pitch = float.Parse(lines[0], CultureInfo.InvariantCulture);
@@ -209,9 +260,9 @@ namespace SimFeedback.telemetry
             telemetryData.Sway = float.Parse(lines[3], CultureInfo.InvariantCulture);
             telemetryData.Surge = float.Parse(lines[5], CultureInfo.InvariantCulture);
             telemetryData.Heave = float.Parse(lines[4], CultureInfo.InvariantCulture);
-            //telemetryData.Heave = float.Parse(lines[8], CultureInfo.InvariantCulture);
-            //telemetryData.AirSpeed = float.Parse(lines[9], CultureInfo.InvariantCulture);
-            //telemetryData.GroundSpeed = float.Parse(lines[10], CultureInfo.InvariantCulture);
+            telemetryData.AirSpeed = float.Parse(lines[9], CultureInfo.InvariantCulture);
+            telemetryData.GroundSpeed = float.Parse(lines[10], CultureInfo.InvariantCulture);
+            telemetryData.isSet = true;
             return telemetryData;
         }
     }
